cb56d668de60ac415444371f99d13db107364d62
Nehad <35563834+81joker@users.noreply.github.com>
2024-04-05 21:07:46 +0200

    Revert "fixe error"

--- js/app.js
+++ js/app.js
@@ -47,22 +47,14 @@ $(document).ready(function () {
       type: $(this).attr("method"),
       url: $(this).attr("action"),
       success: function (response) {
-        console.log(response.success);
         $("#desc").val("");
         $("#dattag").val("");
         $("#datmonat").val("");
         $("#datyear").val("");
         $("#ernnerung").val("");
         AJAXSuccessSubmitSave(response);
-        if (response.success) {
-        displayMessage("successfully addded");
-      } else {
-        displayMessage("Element Not addded", false);
-      }
       },
-       error: (data) => {
-      console.log(data);
-    },
+      error: AJAXErrorSubmitSave,
       dataType: "json",
       timeout: 3000,
     });
@@ -73,7 +65,7 @@ $(document).ready(function () {
   function AJAXSuccessSubmitSave(data) {
     //alert('congrats we send email successfully' + data.mail);
     //  console.log(data.dataneeded);
-    // console.log(data);
+    //  console.log(data);
     let request_result = data.success;
     if (request_result) {
       let tboadyinsert = document.getElementById("data_insert");
@@ -145,6 +137,7 @@ $(document).ready(function () {
     }
 
     // send email
+
     $.ajax({
       data: {
         day: data.dataneeded[0],
@@ -154,22 +147,17 @@ $(document).ready(function () {
         fulldate: data.dataneeded[4],
         email: data.dataneeded[5],
       },
-      // url: "menuüpunkt1.php",
-      // type: "POST",
-      success: function (response) {
-        alert(response)
-        AJAXSuccessEmail
-      },
-      success: function (response) {
-        AJAXErrorEmail(response)
-      },
+      type: "POST",
+      url: "include/updatedmail.php",
+      success: AJAXSuccessEmail,
+      error: AJAXErrorEmail,
       dataType: "json",
     });
     function AJAXSuccessEmail() {
       displayMessage("Mail Sent Successfully");
     }
-    function AJAXErrorEmail(response) {
-      displayMessage("We could not sent mail" + response);
+    function AJAXErrorEmail() {
+      displayMessage("We could not sent mail");
     }
   }
 
